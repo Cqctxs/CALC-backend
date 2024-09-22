c668e14664078de1ed04115ce7bc4d222d17f15d
Sean <seanzhao2007@gmail.com>
2024-09-22 17:16:33 -0400

    Added functions for ease of use - up to question 12

--- controllers/calcController.js
+++ controllers/calcController.js
@@ -1,30 +1,60 @@
-const mathjs = require('mathjs');
+const mathjs = require("mathjs");
 const config = {
   number: "Fraction",
 };
 const math = mathjs.create(mathjs.all, config);
 
-const convert = ([x, y, z]) => {
+const formatVector = ([x, y, z]) => {
   let displayX = "";
   let displayY = "";
   let displayZ = "";
   if (x.d === 1) {
-    displayX = `${x.n*x.s}`;
+    displayX = `${x.n * x.s}`;
   } else {
-    displayX = `${x.n*x.s}/${x.d}`;
+    displayX = `${x.n * x.s}/${x.d}`;
   }
   if (y.d === 1) {
-    displayY = `${y.n*y.s}`;
+    displayY = `${y.n * y.s}`;
   } else {
-    displayY = `${y.n*y.s}/${y.d}`;
+    displayY = `${y.n * y.s}/${y.d}`;
   }
   if (z.d === 1) {
-    displayZ = `${z.n*z.s}`;
+    displayZ = `${z.n * z.s}`;
   } else {
-    displayZ = `${z.n*z.s}/${z.d}`;
+    displayZ = `${z.n * z.s}/${z.d}`;
   }
   return `(${displayX}, ${displayY}, ${displayZ})`;
-}
+};
+
+const formatCartesian = ([a, b, c], d) => {
+  const lcm = math.lcm(a.d, math.lcm(b.d, math.lcm(c.d, d.d)));
+  const newN = math.multiply([a, b, c], lcm);
+  const newD = math.multiply(d, lcm);
+  return `${newN[0].n}x + ${newN[1].n}y + ${newN[2].n}z - ${newD.n} = 0`;
+};
+
+const simplifyDirectionVector = ([x, y, z]) => {
+  const lcm = math.lcm(x.d, math.lcm(y.d, z.d));
+  const multipliedX = math.multiply(x, lcm);
+  const multipliedY = math.multiply(y, lcm);
+  const multipliedZ = math.multiply(z, lcm);
+  const gcd = math.gcd(multipliedX.n, math.gcd(multipliedY.n, multipliedZ.n));
+  const newX = math.divide(multipliedX, gcd);
+  const newY = math.divide(multipliedY, gcd);
+  const newZ = math.divide(multipliedZ, gcd);
+  return [newX, newY, newZ];
+};
+
+const solveLine = ([x1, y1, z1], [ux, uy, uz], [x2, y2, z2], [vx, vy, vz]) => {
+  const solution = math.lusolve(
+    [
+      [ux, -vx],
+      [uy, -vy],
+    ],
+    [x2 - x1, y2 - y1]
+  );
+  return solution;
+};
 
 const calc = async (req, res) => {
   const studentNum = req.body.studentNum;
@@ -77,29 +107,56 @@ const calc = async (req, res) => {
   // 6. Volume OABC
   const volume = math.divide(math.dot(math.cross(A, B), C), 6);
 
-  console.log("volume: ", volume);
-
-  // Median A
-
+  // 7. Median A
   const MBC = math.divide(BC, 2);
-  const AMBC = math.subtract(MBC, A);
-  const medianA = `L1: ${convert(A)} + t${convert(AMBC)}; t ∈ R`;
+  const dirCentroidA = simplifyDirectionVector(math.subtract(MBC, A));
+  const medianA = `L1: ${formatVector(A)} + s${formatVector(dirCentroidA)}; s ∈ R`;
 
   // Median B
-
   const MAC = math.divide(AC, 2);
-  const BMAC = math.subtract(MAC, B);
-  const medianB = `L2: ${convert(B)} + t${convert(BMAC)}; t ∈ R`;
+  const dirCentroidB = simplifyDirectionVector(math.subtract(MAC, B));
+  const medianB = `L2: ${formatVector(B)} + t${formatVector(dirCentroidB)}; t ∈ R`;
 
   // Median C
-
   const MAB = math.divide(AB, 2);
-  const CMAB = math.subtract(MAB, C);
-  const medianC = `L3: ${convert(C)} + t${convert(CMAB)}; t ∈ R`;
+  const dirCentroidC = simplifyDirectionVector(math.subtract(MAB, C));
+  const medianC = `L3: ${formatVector(C)} + u${formatVector(dirCentroidC)}; u ∈ R`;
 
-  console.log("medianA: ", medianA);
-  console.log("medianB: ", medianB);
-  console.log("medianC: ", medianC);
+  // 8. Centroid
+  let sol = solveLine(A, dirCentroidA, B, dirCentroidB);
+  let s = sol[0][0];
+  let t = sol[1][0];
+  const G = math.add(A, math.multiply(dirCentroidA, s));
+
+  // 9. Cartesian equation of plane ABC
+  const n = math.cross(AB, AC);
+  const D = math.dot(n, A);
+
+  const plane = formatCartesian(n, D);
+  console.log(plane)
+
+  // 10. Distance from O to plane ABC
+  const distNum = math.abs(math.dot(n, A));
+  const distDenInRoot = math.square(n[0]) + math.square(n[1]) + math.square(n[2]);
+
+  // 11. Altitude from A to BC
+  const dirAltitudeA = simplifyDirectionVector(math.cross(n, BC));
+  const altitudeA = `L4: ${formatVector(A)} + s${formatVector(dirAltitudeA)}; s ∈ R`;
+
+  // Altitude form B to AC
+  const dirAltitudeB = simplifyDirectionVector(math.cross(n, AC));
+  const altitudeB = `L5: ${formatVector(B)} + t${formatVector(dirAltitudeB)}; t ∈ R`;
+
+  // Altitude from C to AB
+  const dirAltitudeC = simplifyDirectionVector(math.cross(n, AB));
+  const altitudeC = `L6: ${formatVector(C)} + u${formatVector(dirAltitudeC)}; u ∈ R`;
+
+  // 12. Orthrocenter
+  sol = solveLine(A, dirAltitudeA, B, dirAltitudeB);
+  s = sol[0][0];
+  t = sol[1][0];
+  const H = math.add(A, math.multiply(dirAltitudeA, s));
+  console.log(formatVector(H));
 
   res.status(201).json({ success: "route found" });
 };
